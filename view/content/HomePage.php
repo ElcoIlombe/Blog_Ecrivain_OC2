a06f8c8f08ab5afbe6e20e4ba6543a8d10a194ac
Jonathan <jonathan.ilombe@hotmail.com>
2017-11-17 20:54:40 +0100

    Création du contenu de la homePage ainsi et configuration de template.php

--- view/content/HomePage.php
+++ view/content/HomePage.php
@@ -1,7 +1,26 @@
 <?php $title = "Jean Forteroche"; ?>
 
 <?php ob_start(); ?>
-<h1> Billet Simple pour l'Alaska </h1>
+<nav>
+	<h1> Jean Forteroche </h1>
+	<ul>
+		<li> Accueil </li>
+		<li> Histoire </li>
+		<li> Connexion Admin.</li>
+	</ul>
+</nav>
+<div class="left_side">
+	<img >
+	<div class="presentation"></div>
+	<div class="news"></div>
+</div>
+<div class="right_side">
+	<div></div>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/template.php'); ?>
 
 
 
